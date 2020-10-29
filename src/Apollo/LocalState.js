a39ceebb81e6500671fd014926963c30619a70f6
Joonsoo Kim <daniel.kim365@hotmail.com>
2020-10-29 11:57:28 -0400

    publish

--- src/Apollo/LocalState.js
+++ src/Apollo/LocalState.js
@@ -1,22 +1,21 @@
 export const defaults = {
-    isLoggedIn: Boolean(localStorage.getItem("token")) || false
-  };
-  
-  export const resolvers = {
-    Mutation: {
-      logUserIn: (_, { token }, { cache }) => {
-        localStorage.setItem("token", token);
-        cache.writeData({
-          data: {
-            isLoggedIn: true
-          }
-        });
-        return null;
-      },
-      logUserOut: (_, __, { cache }) => {
-        localStorage.removeItem("token");
-        window.location.reload();
-        return null;
-      }
+  isLoggedIn: Boolean(localStorage.getItem("token")) || false
+};
+export const resolvers = {
+  Mutation: {
+    logUserIn: (_, { token }, { cache }) => {
+      localStorage.setItem("token", token);
+      cache.writeData({
+        data: {
+          isLoggedIn: true
+        }
+      });
+      return null;
+    },
+    logUserOut: (_, __, { cache }) => {
+      localStorage.removeItem("token");
+      window.location = "/";
+      return null;
     }
-  };
+  }
+};
