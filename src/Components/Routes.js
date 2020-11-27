57f59e9d9bbf5159e2d968838a77ef0c90b00d9d
Joonsoo Kim <daniel.kim365@hotmail.com>
2020-11-27 15:12:19 -0500

    changed mbti folder

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -15,7 +15,7 @@ const LoggedInRoutes = () => (
     <Route path="/explore" component={Explore} />
     <Route path="/search" component={Search} />
     <Route path="/:username" component={Profile} />
-    <Route exact path="/mbti" component={Mbti} />
+    <Route path="/mbti" component={Mbti} />
     <Route path="/explorepost" component={ExplorerPost} />
     <Redirect from="*" to="/" />
   </Switch>
