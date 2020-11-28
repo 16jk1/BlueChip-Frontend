00b5b483c45a6a55f2400db9d6915c471832255f
Joonsoo Kim <daniel.kim365@hotmail.com>
2020-11-27 21:44:22 -0500

    changed mbti folder

--- src/Routes/mbti/mbti.js
+++ src/Routes/mbti/mbti.js
@@ -2,11 +2,62 @@ import React, { Component } from 'react'
 import anime from 'animejs'
 import Intro from './components/Intro'
 import Question from './components/Question'
+import styled from "styled-components";
+import Theme from "../../Styles/Theme";
+
+const Wrapper = styled.div`
+  min-height: 90vh;
+  ${Theme.router};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 80vh;
+`;
 
 
+class mbti extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showQuestion: false
+    }
+    this._onStartClick = this._onStartClick.bind(this)
+  }
 
-export default () => {
-  return (
-  <Intro _onStartClick={this._onStartClick} title="Welcome To Personality Test" />
-  )
+  renderIntro() {
+    return <Intro _onStartClick={this._onStartClick} title="Welcome To Personality Test" />
+  }
+
+  renderQuestion() {
+    return <Question />
+  }
+
+  render() {
+    let showQuestion = this.state.showQuestion
+    if (showQuestion) {
+      return this.renderQuestion()
+    }
+    return this.renderIntro()
+  }
+
+  _onStartClick() {
+    this.animateOut()
+    setTimeout(() => this.setState({ showQuestion: !this.state.showQuestion }), 1000)
+  }
+
+  animateOut() {
+    setTimeout(
+      () =>
+        anime({
+          targets: '.card',
+          translateX: '150%',
+          elasticity: function(el, i, l) {
+            return 200 + i * 200
+          }
+        }),
+      200
+    )
+  }
 }
+
+export default mbti
